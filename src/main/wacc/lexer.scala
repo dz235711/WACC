79254f3c0b4d017187764c9a13ea49c5d558586c
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2025-01-20 12:37:48 +0000

    chore: add formatter

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -5,12 +5,12 @@ import parsley.token.Lexer
 import parsley.token.descriptions.*
 
 object lexer {
-    private val desc = LexicalDesc.plain.copy(
-        // your configuration goes here
-    )
-    private val lexer = Lexer(desc)
+  private val desc = LexicalDesc.plain.copy(
+    // your configuration goes here
+  )
+  private val lexer = Lexer(desc)
 
-    val integer = lexer.lexeme.integer.decimal
-    val implicits = lexer.lexeme.symbol.implicits
-    def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
+  val integer = lexer.lexeme.integer.decimal
+  val implicits = lexer.lexeme.symbol.implicits
+  def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -7,14 +7,14 @@ import lexer.implicits.implicitSymbol
 import lexer.{integer, fully}
 
 object parser {
-    def parse(input: String): Result[String, BigInt] = parser.parse(input)
-    private val parser = fully(expr)
-    
-    private val add = (x: BigInt, y: BigInt) => x + y
-    private val sub = (x: BigInt, y: BigInt) => x - y
+  def parse(input: String): Result[String, BigInt] = parser.parse(input)
+  private val parser = fully(expr)
 
-    private lazy val expr: Parsley[BigInt] =
-        chain.left1(integer | "(" ~> expr <~ ")")(
-            ("+" as add) | ("-" as sub)
-        )
+  private val add = (x: BigInt, y: BigInt) => x + y
+  private val sub = (x: BigInt, y: BigInt) => x - y
+
+  private lazy val expr: Parsley[BigInt] =
+    chain.left1(integer | "(" ~> expr <~ ")")(
+      ("+" as add) | ("-" as sub)
+    )
 }
