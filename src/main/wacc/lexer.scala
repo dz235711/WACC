72cc73d21df4721b9807fcd8e008d4fec8ee4a54
Dingyan <dz1223@ic.ac.uk>
2025-01-21 18:29:56 +0000

    feat: completed lexer descriptions
    Co-authored-by: Rukaiya Khondaker <rnk23@ic.ac.uk>

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -1,12 +1,32 @@
 package wacc
 
 import parsley.Parsley
-import parsley.token.Lexer
+import parsley.token.{Lexer, Basic}
 import parsley.token.descriptions.*
 
 object lexer {
   private val desc = LexicalDesc.plain.copy(
-    // your configuration goes here
+    nameDesc = NameDesc.plain.copy(
+      identifierStart = Basic(c => c.isLetter || c == '_'),
+      identifierLetter = Basic(c => c.isLetterOrDigit || c == '_')
+    ),
+    spaceDesc = SpaceDesc.plain.copy(
+      lineCommentStart = "#"
+    ),
+    symbolDesc = SymbolDesc.plain.copy(
+      hardKeywords = Set("begin", "end", "is", "skip", "read", "free", "return", "exit", "print", "println", "if", "then", "else", "fi", "while", "do", "done", "newpair", "call", "fst", "snd"),
+      hardOperators = Set("!", "-", "len", "ord", "chr", "*", "/", "%", "+", "-", ">", ">=", "<", "<=", "==", "!=", "&&", "||"),
+    ),
+    textDesc = TextDesc.plain.copy(
+      graphicCharacter = Basic(c => 31 < c.toInt && c.toInt < 127 && !Set('\\', '\'', '"').contains(c)),
+      escapeSequences = EscapeDesc.plain.copy(
+        literals = Set('0', 'b', 't', 'n', 'f', 'r', '"', '\'', '\\'),
+      ),
+    ),
+    numericDesc = NumericDesc.plain.copy(
+      integerNumbersCanBeHexadecimal = false,
+      integerNumbersCanBeOctal = false,
+    ),
   )
   private val lexer = Lexer(desc)
 
