50e8afc2edff829e485fb6f243375a046a28372f
Rukaiya Khondaker <rnk23@ic.ac.uk>
2025-01-27 12:46:47 +0000

    feat: add precedence for binary operators
    Co-authored-by: Alexander Biraben-Renard <ab1223@ic.ac.uk>

--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -2,8 +2,9 @@ package wacc
 
 import parsley.{Parsley, Result}
 import parsley.quick.*
-import parsley.expr.chain
+import parsley.expr.{chain, precedence, Ops, InfixL, Prefix, InfixR, InfixN}
 import parsley.syntax.zipped.*
+import parsley.generic.*
 
 import lexer.implicits.implicitSymbol
 import lexer.{int, bool, char, str, pair, ident, fully}
@@ -12,62 +13,93 @@ object parser {
   def parse(input: String): Result[String, Program] = parser.parse(input)
   private val parser = fully(prog)
 
-  private lazy val expr: Parsley[Expr] = ???
-  private lazy val atom: Parsley[Expr] = int.map(IntLiter.apply) 
-                                         | atomic(bool.map(BoolLiter.apply)) 
-                                         | atomic(char.map(CharLiter.apply))
-                                         | atomic(str.map(StringLiter.apply))
-                                         | atomic(pair as PairLiter)
-                                         | atomic(ident.map(Ident.apply))
-                                         | arrayElem 
-                                         | "(" ~> expr <~ ")"
-  private lazy val arrayElem: Parsley[ArrayElem] = (ident.map(Ident.apply), some(expr)).zipped(ArrayElem(_, _))
-  private lazy val unaryOper = ???
-  private lazy val binaryOper = ???
-  
-  private lazy val _type: Parsley[Type] = baseType 
-                                          | arrayType 
-                                          | pairType
-  private lazy val baseType: Parsley[BaseType] = ("int" as BaseType.Int) 
-                                                 | ("bool" as BaseType.Bool) 
-                                                 | ("char" as BaseType.Char) 
-                                                 | "string" as BaseType.String
-  private lazy val arrayType: Parsley[ArrayType] = ???
-  private lazy val pairType: Parsley[PairType] = ("pair(" ~> pairElemType <~ ",", pairElemType <~ ")").zipped(PairType(_, _))
-  private lazy val pairElemType: Parsley[PairElemType] = baseType 
-                                                         | arrayType 
-                                                         | "pair" as ErasedPair
+  private lazy val expr: Parsley[Expr] =
+    precedence(
+      IntLiter(int),
+      BoolLiter(bool),
+      CharLiter(char),
+      StringLiter(str),
+      pair ~> pure(PairLiter),
+      Ident(ident),
+      ArrayElem(Ident(ident), some("[" ~> expr <~ "]")),
+      "(" ~> expr <~ ")"
+    )(
+      Ops(Prefix)(
+        Not <# "!",
+        Negate <# "-",
+        Len <# "len",
+        Ord <# "ord",
+        Chr <# "chr"
+      ),
+      Ops(InfixL)(Mult <# "*", Mod <# "%", Div <# "/"),
+      Ops(InfixL)(Add <# "+", Sub <# "-"),
+      Ops(InfixN)(
+        Greater <# ">",
+        GreaterEq <# ">=",
+        Smaller <# "<",
+        SmallerEq <# "<="
+      ),
+      Ops(InfixN)(Equals <# "==", NotEquals <# "!="),
+      Ops(InfixR)(And <# "&&"),
+      Ops(InfixR)(Or <# "||")
+    )
 
-  private lazy val prog: Parsley[Program] = ("begin" ~> many(func), stmt <~ "end").zipped(Program(_, _))
-  private lazy val func: Parsley[Func] = (_type, ident.map(Ident.apply), "(" ~> paramList <~ ")", "is" ~> stmt <~ "end").zipped(Func(_, _, _, _))
-  private lazy val paramList: Parsley[List[(Type, Ident)]] = (param <::> many("," ~> param))
-  private lazy val param: Parsley[(Type, Ident)] = _type <~> ident.map(Ident.apply)
-  private lazy val stmt: Parsley[Stmt] = chain.left1(("skip" as Skip)
-                                                     | (_type, ident.map(Ident.apply), "=" ~> rvalue).zipped(Decl(_, _, _))
-                                                     | (_type, ident.map(Ident.apply), "=" ~> rvalue).zipped(Decl(_, _, _))
-                                                     | (lvalue, "=" ~> rvalue).zipped(Asgn(_, _))
-                                                     | "read" ~> lvalue.map(Read.apply)
-                                                     | "free" ~> expr.map(Free.apply)
-                                                     | "return" ~> expr.map(Return.apply)
-                                                     | "exit" ~> expr.map(Exit.apply)
-                                                     | "print" ~> expr.map(Print.apply)
-                                                     | "println" ~> expr.map(PrintLn.apply)
-                                                     | ("if" ~> expr, "then" ~> stmt, "else" ~> stmt <~ "fi").zipped(If(_, _, _))
-                                                     | ("while" ~> expr, "do" ~> stmt <~ "done").zipped(While(_, _))
-                                                     | "begin" ~> stmt <~ "end")(";" as Semi.apply)
-  private lazy val lvalue: Parsley[LValue] = ident.map(Ident.apply) 
-                                           | arrayElem 
-                                           | pairElem
-  private lazy val rvalue: Parsley[RValue] = expr 
-                                             | arrayLiter 
-                                             | "newpair(" ~> expr <~ "," ~> expr <~ ")"
-                                             | "call" ~> ident.map(Ident.apply) <~ "(" ~> argList <~ ")"
+  private lazy val _type: Parsley[Type] = baseType
+    | arrayType
+    | pairType
+  private lazy val baseType: Parsley[BaseType] = ("int" as BaseType.Int)
+    | ("bool" as BaseType.Bool)
+    | ("char" as BaseType.Char)
+    | "string" as BaseType.String
+  private lazy val arrayType: Parsley[ArrayType] = ???
+  private lazy val pairType: Parsley[PairType] =
+    ("pair(" ~> pairElemType <~ ",", pairElemType <~ ")").zipped(PairType(_, _))
+  private lazy val pairElemType: Parsley[PairElemType] = baseType
+    | arrayType
+    | "pair" as ErasedPair
+
+  private lazy val prog: Parsley[Program] =
+    ("begin" ~> many(func), stmt <~ "end").zipped(Program(_, _))
+  private lazy val func: Parsley[Func] = (
+    _type,
+    ident.map(Ident.apply),
+    "(" ~> paramList <~ ")",
+    "is" ~> stmt <~ "end"
+  ).zipped(Func(_, _, _, _))
+  private lazy val paramList: Parsley[List[(Type, Ident)]] =
+    (param <::> many("," ~> param))
+  private lazy val param: Parsley[(Type, Ident)] =
+    _type <~> ident.map(Ident.apply)
+  private lazy val stmt: Parsley[Stmt] = chain.left1(
+    ("skip" as Skip)
+      | (_type, ident.map(Ident.apply), "=" ~> rvalue).zipped(Decl(_, _, _))
+      | (_type, ident.map(Ident.apply), "=" ~> rvalue).zipped(Decl(_, _, _))
+      | (lvalue, "=" ~> rvalue).zipped(Asgn(_, _))
+      | "read" ~> lvalue.map(Read.apply)
+      | "free" ~> expr.map(Free.apply)
+      | "return" ~> expr.map(Return.apply)
+      | "exit" ~> expr.map(Exit.apply)
+      | "print" ~> expr.map(Print.apply)
+      | "println" ~> expr.map(PrintLn.apply)
+      | ("if" ~> expr, "then" ~> stmt, "else" ~> stmt <~ "fi")
+        .zipped(If(_, _, _))
+      | ("while" ~> expr, "do" ~> stmt <~ "done").zipped(While(_, _))
+      | "begin" ~> stmt <~ "end"
+  )(";" as Semi.apply)
+  private lazy val lvalue: Parsley[LValue] = ident.map(Ident.apply)
+    | arrayElem
+    | pairElem
+  private lazy val rvalue: Parsley[RValue] = expr
+    | arrayLiter
+    | "newpair(" ~> expr <~ "," ~> expr <~ ")"
+    | "call" ~> ident.map(Ident.apply) <~ "(" ~> argList <~ ")"
   private lazy val argList: Parsley[List[Expr]] = (expr <::> many("," ~> expr))
   private lazy val pairElem: Parsley[LValue] = ("fst" ~> lvalue).map(Fst.apply)
-                                               | ("snd" ~> lvalue).map(Snd.apply)
-  private lazy val arrayLiter: Parsley[ArrayLiter] = "[" ~> (expr <::> many("," ~> expr)).map(ArrayLiter(_))
-                                                             | ("" as ArrayLiter(List())) <~ "]"
-  
+    | ("snd" ~> lvalue).map(Snd.apply)
+  private lazy val arrayLiter: Parsley[ArrayLiter] =
+    "[" ~> (expr <::> many("," ~> expr)).map(ArrayLiter(_))
+      | ("" as ArrayLiter(List())) <~ "]"
+
   // private val add = (x: BigInt, y: BigInt) => x + y
   // private val sub = (x: BigInt, y: BigInt) => x - y
 
--- src/main/wacc/syntax.scala
+++ src/main/wacc/syntax.scala
@@ -1,12 +1,14 @@
 package wacc
 
+import parsley.generic
+
 sealed trait Type
 sealed trait PairElemType
 enum BaseType extends Type, PairElemType {
-    case Int
-    case Bool
-    case Char
-    case String
+  case Int
+  case Bool
+  case Char
+  case String
 }
 case class ArrayType(t: Type) extends Type, PairElemType
 case class PairType(t1: PairElemType, t2: PairElemType) extends Type
@@ -14,41 +16,65 @@ object ErasedPair extends PairElemType
 
 sealed trait Expr extends RValue
 case class Not(e: Expr) extends Expr
+object Not extends generic.ParserBridge1[Expr, Expr]
 case class Negate(e: Expr) extends Expr
+object Negate extends generic.ParserBridge1[Expr, Expr]
 case class Len(e: Expr) extends Expr
+object Len extends generic.ParserBridge1[Expr, Expr]
 case class Ord(e: Expr) extends Expr
+object Ord extends generic.ParserBridge1[Expr, Expr]
 case class Chr(e: Expr) extends Expr
+object Chr extends generic.ParserBridge1[Expr, Expr]
 
 case class Mult(e1: Expr, e2: Expr) extends Expr
+object Mult extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Div(e1: Expr, e2: Expr) extends Expr
+object Div extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Mod(e1: Expr, e2: Expr) extends Expr
+object Mod extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Add(e1: Expr, e2: Expr) extends Expr
+object Add extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Sub(e1: Expr, e2: Expr) extends Expr
+object Sub extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Greater(e1: Expr, e2: Expr) extends Expr
+object Greater extends generic.ParserBridge2[Expr, Expr, Expr]
 case class GreaterEq(e1: Expr, e2: Expr) extends Expr
+object GreaterEq extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Smaller(e1: Expr, e2: Expr) extends Expr
+object Smaller extends generic.ParserBridge2[Expr, Expr, Expr]
 case class SmallerEq(e1: Expr, e2: Expr) extends Expr
+object SmallerEq extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Equals(e1: Expr, e2: Expr) extends Expr
+object Equals extends generic.ParserBridge2[Expr, Expr, Expr]
 case class NotEquals(e1: Expr, e2: Expr) extends Expr
+object NotEquals extends generic.ParserBridge2[Expr, Expr, Expr]
 case class And(e1: Expr, e2: Expr) extends Expr
+object And extends generic.ParserBridge2[Expr, Expr, Expr]
 case class Or(e1: Expr, e2: Expr) extends Expr
+object Or extends generic.ParserBridge2[Expr, Expr, Expr]
 
 case class IntLiter(x: Int) extends Expr
+object IntLiter extends generic.ParserBridge1[Int, Expr]
 case class BoolLiter(b: Boolean) extends Expr
+object BoolLiter extends generic.ParserBridge1[Boolean, Expr]
 case class CharLiter(c: Char) extends Expr
+object CharLiter extends generic.ParserBridge1[Char, Expr]
 case class StringLiter(s: String) extends Expr
-object PairLiter extends Expr
-case class Ident(v: String) extends Expr, LValue
-// Not an actual list of expression, but rather the list of indexing expression. E.g. x[e0][e1][e2].
-case class ArrayElem(v: Ident, es: List[Expr]) extends Expr, LValue
+object StringLiter extends generic.ParserBridge1[String, Expr]
+case object PairLiter extends Expr, generic.ParserBridge0[Expr]
+case class Ident(v: String) extends Expr with LValue
+object Ident extends generic.ParserBridge1[String, Expr]
+case class ArrayElem(v: Ident, es: List[Expr]) extends Expr with LValue
+object ArrayElem extends generic.ParserBridge2[Ident, List[Expr], Expr]
 case class NestedExpr(e: Expr) extends Expr
+object NestedExpr extends generic.ParserBridge1[Expr, Expr]
 
 sealed trait LValue
 
 sealed trait RValue
 case class ArrayLiter(es: List[Expr]) extends RValue
 case class NewPair(e1: Expr, e2: Expr) extends RValue
-case class Fst(l: LValue)  extends LValue, RValue
+case class Fst(l: LValue) extends LValue, RValue
 case class Snd(l: LValue) extends LValue, RValue
 case class Call(v: Ident, args: List[Expr]) extends RValue
 
