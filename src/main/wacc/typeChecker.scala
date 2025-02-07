c859b77710aacb2a5e5ee8e57a0114365957e557
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2025-02-07 11:00:16 +0000

    chore: match on type instead of pattern

--- src/main/wacc/typeChecker.scala
+++ src/main/wacc/typeChecker.scala
@@ -55,10 +55,10 @@ sealed class TypeChecker {
           None
         }
       case (?, _) => Some(?) // Unconstrained types satisfy all constraints
-      case (kty: Array, IsArray)            => Some(kty)
-      case (kty: Pair, IsPair)              => Some(kty)
-      case (kty @ (Int | Char), IsReadable) => Some(kty)
-      case (_, IsReadable)                  =>
+      case (kty: Array, IsArray)           => Some(kty)
+      case (kty: Pair, IsPair)             => Some(kty)
+      case (kty: (Int | Char), IsReadable) => Some(kty)
+      case (_, IsReadable)                 =>
         // TODO: Error handling - tried to read a non-readable type
         println("tried to read a non-readable type")
         None
