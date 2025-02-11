e9ee6ccec46ebaa3329219c053044347a645a262
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2025-02-11 17:17:19 +0000

    refactor: extract runFrontend to file
    also improves visual quality of verbose messages

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -1,115 +1,57 @@
 package wacc
 
-import parsley.{Success, Failure}
-import scala.sys.exit
 import java.io.FileNotFoundException
-import WaccErrorBuilder.{format, setLines}
+import scala.sys.exit
 
-import parsley.errors.ErrorBuilder
-
-/** Manages syntax parsing and semantic analysis of a WACC program
-   *
-   * @param args  Cmdline arguments
-   * @return (Exit code, Either[Exit message, List of errors])
-   */
-def runFrontend(args: Array[String]): (Int, Either[String, List[WaccError]]) = {
-  // Determine whether to pretty print output
-  val verbose = args.contains("--verbose") || args.contains("-v")
-
-  // Match the first cmdline argument and try to parse it as file path
-  args.headOption match {
-    case Some(path) =>
-      try {
-        // Get file handle
-        val source = io.Source.fromFile(path)
-
-        // Read first as list so we can later set the lines in errors by index
-        val listifiedLines = source.getLines().toList
-
-        // Copy to a string to pass through parser
-        val lines = listifiedLines.mkString("\n")
-        source.close()
-
-        // Prase the file
-        if (verbose)
-          println(
-            "\n------------------------------ Input File ------------------------------"
-          )
-          println(lines)
-          println(
-            "------------------------------ /Input File ------------------------------\n"
-          )
-        given ErrorBuilder[WaccError] = new WaccErrorBuilder
-        given errCtx: ErrorContext = new ErrorContext
-        parser.parse(lines) match {
-          case Success(x) =>
-            // Successfully parsed so we continue to semantic analysis
-            if (verbose)
-              println(
-                "\n------------------------------ Pretty-Printed AST ------------------------------"
-              )
-              println(prettyPrint(x))
-              println(
-                "------------------------------ /Pretty-Printed AST ------------------------------\n"
-              )
-
-            // Semantic analysis
-            val typedAST = TypeChecker().checkProg(Renamer.rename(x))
-
-            if (verbose) {
-              println(
-                "\n------------------------------ Typed AST ------------------------------"
-              )
-              println(typedAST)
-              println(
-                "------------------------------ /Typed AST ------------------------------\n"
-              )
-            }
-
-            // Convert list buffer to list to allow mapping
-            val listifiedErrs = errCtx.get
-            if (listifiedErrs.nonEmpty)
-              /* Return correctly formatted errors with lines set if error list is nonempty */
-              (
-                200,
-                Right(
-                  listifiedErrs.map(e =>
-                    setLines(
-                      format(e, Some(path), ErrType.Semantic),
-                      listifiedLines
-                    )
-                  )
-                )
-              )
-            else
-              // Otherwise semantic analysis passed
-              (0, Left("Parsed successfully! 🎉"))
-          case Failure(err) =>
-            if (verbose)
-              println("Failed to parse! 😢")
-            // Return correctly formatted syntax error
-            (100, Right(List(format(err, Some(path), ErrType.Syntax))))
-        }
-      } catch {
-        case _: FileNotFoundException => (-1, Left(s"file not found: $path 💀"))
-      }
-    case None => (-2, Left("please enter a file path 😡"))
+/** Reads a file and returns a list of lines
+ *
+ * @param path Path to the file
+ * @return List of lines in the file or None if the file is not found
+ */
+def readFile(path: String): Option[List[String]] = {
+  try {
+    val source = io.Source.fromFile(path)
+    val lines = source.getLines().toList
+    source.close()
+    Some(lines)
+  } catch {
+    case _: FileNotFoundException => None
   }
 }
 
 def main(args: Array[String]): Unit = {
   println("Hello, WACC! 👋😃👍")
-  val (status, output) = runFrontend(args)
+  val verbose = args.contains("--verbose") || args.contains("-v")
 
-  // Print output msg or errors to output stream
-  output match {
-    case Left(msg) => println(msg)
-    case Right(err) =>
-      println(
-        err.foldRight(new StringBuilder)((e, acc) => printWaccError(e, acc))
-      )
+  val path = args.headOption
+
+  // If no file path is provided, print usage message
+  if (path.isEmpty) {
+    println("Usage: wacc <file> [options]")
+    println("Options:")
+    println("  --verbose, -v  Print verbose output")
+    exit(1)
   }
 
-  // Exit with status code
-  exit(status)
+  // Read the file
+  val file = readFile(path.get)
+  if (file.isEmpty) {
+    println(s"Error: File '${path.get}' not found")
+    exit(1)
+  }
+  val lines = file.get
+
+  // Run the frontend
+  runFrontendPending(lines, verbose) match {
+    case Right(program) =>
+      // Run the backend
+      val output = runBackend(program, verbose)
+      println(output)
+      exit(0)
+    case Left((status, output)) =>
+      // Print output msg or errors to output stream
+      println(output.foldRight(new StringBuilder)((e, acc) => printWaccError(e, acc)))
+      // Exit with status code
+      exit(status)
+  }
 }
