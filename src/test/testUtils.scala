a7483b848283dadca06a6a518de72cdef8d5229d
Dingyan <dz1223@ic.ac.uk>
2025-02-12 23:38:31 +0000

    refactor: refactor out magic number into constant and added commenting for program execution function

--- src/test/testUtils.scala
+++ src/test/testUtils.scala
@@ -9,6 +9,8 @@ import org.scalatest.Tag
 object Frontend extends Tag("wacc.Frontend")
 object Backend extends Tag("wacc.Backend")
 
+private val TimeoutCode = 124
+
 private val addrRegex = "0x[0-9a-fA-F]+".r
 private val errRegex = "fatal error:.+".r
 
@@ -32,23 +34,32 @@ private def partiallyCompile[T](path: String, transformer: TypedAST.Program => T
         case Right(prog)     => transformer(prog)
 
 private def runProgram(prog: TypedAST.Program, input: String): String = {
+  // Create temporary file to write compiled assembly into
   val source = new File("test.s")
   source.createNewFile()
+
+  // Write assembly
   val writer = new PrintWriter(source)
   writer.write(runBackend(prog, false))
   writer.flush()
   writer.close()
+
+  // Create the processes to assemble and run the compiled assembly
   val pBuilder = new ProcessBuilder()
   pBuilder.command("gcc", "-o", "test", "-z", "noexecstack", "test.s").start().waitFor()
   val process = pBuilder.command("timeout", "1s", "./test").start()
+  // Feed input to the process
   val iStream = process.getOutputStream()
   process.waitFor()
-  if process.exitValue() == 124 then "!!!process timed out!!!"
+  if process.exitValue() == TimeoutCode then "!!!process timed out!!!"
   else
+    // If the process hasn't t
     iStream.write(input.getBytes())
     iStream.flush()
     val output = process.getInputStream().readAllBytes().mkString
     process.waitFor()
+
+    // Delete the temporary files for assembly code and executable
     source.delete()
     val asmFile = new File("test")
     asmFile.delete()
