b600eb8b9d2c84999cc03b34b0da1dfb517a7198
Rukaiya Khondaker <rnk23@ic.ac.uk>
2025-02-13 00:07:56 +0000

    wip: add arithmetic, logical and bitwise instructions
    Co-authored-by: Lawan Fathullah <lawan.fathullah23@imperial.ac.uk>

--- src/main/wacc/instructions.scala
+++ src/main/wacc/instructions.scala
@@ -25,3 +25,29 @@ case class RegScaleRegImmPointer(reg1: Register, scale: Int, reg2: Register, imm
 
 /** Pointer for `[scale * reg + imm]` */
 case class ScaleRegImmPointer(scale: Int, reg: Register, imm: Immediate)(val size: Size) extends Pointer
+
+sealed trait Instruction
+
+// Arithmetic instructions
+case class AddCarry(dest: Register | Pointer, src: Immediate | Register | Pointer) extends Instruction
+case class Add(dest: Register | Pointer, src: Immediate | Register | Pointer) extends Instruction
+case class Dec(dest: Register | Pointer) extends Instruction
+case class Inc(dest: Register | Pointer) extends Instruction
+case class Div(src: Register | Pointer) extends Instruction
+case class SignedDiv(src: Register | Pointer) extends Instruction
+case class Mul(src: Register | Pointer) extends Instruction
+// TODO: IMUL with default values
+case class Neg(dest: Register | Pointer) extends Instruction
+case class Not(dest: Register | Pointer) extends Instruction
+case class Sub(dest: Register | Pointer, src: Immediate | Register | Pointer) extends Instruction
+
+// Logical and bitwise instrucitons
+case class And(dest: Register | Pointer, src: Immediate | Register | Pointer) extends Instruction
+case class Or(dest: Register | Pointer, src: Immediate | Register | Pointer) extends Instruction
+case class Xor(dest: Register | Pointer, src: Immediate | Register | Pointer) extends Instruction
+case class ShiftArithLeft(dest: Register | Pointer, count: Immediate) extends Instruction
+case class ShiftArithRight(dest: Register | Pointer, count: Immediate) extends Instruction
+case class ShiftLogicalLeft(dest: Register | Pointer, count: Immediate) extends Instruction
+case class ShiftLogicalRight(dest: Register | Pointer, count: Immediate) extends Instruction
+case class Test(src1: Register | Pointer, src2: Immediate | Register) extends Instruction
+case class Compare(src1: Register | Pointer, src2: Immediate | Register | Pointer) extends Instruction
