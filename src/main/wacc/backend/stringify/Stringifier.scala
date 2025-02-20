281fb388ea1586d4effdd66b3c82351d0352f727
Dingyan <dz1223@ic.ac.uk>
2025-02-20 15:10:38 +0000

    feat: translate registers

--- src/main/wacc/backend/stringify/Stringifier.scala
+++ src/main/wacc/backend/stringify/Stringifier.scala
@@ -2,8 +2,6 @@ package wacc
 
 import wacc.Size.*
 
-import scala.collection.immutable.Map
-
 class Stringifier {
   def stringify(instructions: List[Instruction]): String = {
     given stringCtx: ListContext[String] = new ListContext()
@@ -41,12 +39,48 @@ class Stringifier {
     case Call("exit@plt") =>
       flagCtx.add("exit", true)
       stringCtx.add(s"call _exit")
-    case Call("puts@plt") => stringCtx.add(s"call _println")
+    case Call("scanf@plt") =>
+      flagCtx.add("readi", true)
+      stringCtx.add(s"call _readi")
   }
 
   private def stringifyOperand(operand: Register | Pointer | Immediate | String): String = operand match {
     case n: Immediate => n.toString
-    case RDI(W32)     => "edi"
+    case r: Register  => stringifyRegister(r)
+    case s: String    => s
+  }
+
+  def stringifyRegister(register: Register): String = register match {
+    case RAX(s) => prependSize(s, "AX", false)
+    case RBX(s) => prependSize(s, "BX", false)
+    case RCX(s) => prependSize(s, "CX", false)
+    case RDX(s) => prependSize(s, "DX", false)
+    case RSI(s) => prependSize(s, "SI")
+    case RDI(s) => prependSize(s, "DI")
+    case RSP(s) => prependSize(s, "SP")
+    case RBP(s) => prependSize(s, "BP")
+    case R8(s)  => appendSize(s, "R8")
+    case R9(s)  => appendSize(s, "R9")
+    case R10(s) => appendSize(s, "R10")
+    case R11(s) => appendSize(s, "R11")
+    case R12(s) => appendSize(s, "R12")
+    case R13(s) => appendSize(s, "R13")
+    case R14(s) => appendSize(s, "R14")
+    case R15(s) => appendSize(s, "R15")
+  }
+
+  private def prependSize(size: Size, reg: String, keepTail: Boolean = true): String = size match {
+    case W8  => (if keepTail then reg else reg.slice(0, 1)) + "L"
+    case W16 => reg
+    case W32 => "E" + reg
+    case W64 => "R" + reg
+  }
+
+  private def appendSize(size: Size, reg: String): String = size match {
+    case W8  => reg + "B"
+    case W16 => reg + "W"
+    case W32 => reg + "D"
+    case W64 => reg
   }
 
   private def addExit(using ctx: ListContext[String]): Unit = {
