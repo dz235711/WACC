1724d36806760561126080baff24d3dab88f5524
Dingyan <dz1223@ic.ac.uk>
2025-02-28 11:43:26 +0000

    test: pass io tests

--- src/test/wacc/backend/BackendValidIoTests.scala
+++ src/test/wacc/backend/BackendValidIoTests.scala
@@ -6,20 +6,20 @@ import org.scalatest.matchers.should.Matchers.*
 class BackendValidIoTests extends AnyFlatSpec {
   val dir = "src/test/examples/valid/IO/"
 
-  it should "execute IOLoop.wacc" taggedAs Backend in pending /*{
+  it should "execute IOLoop.wacc" taggedAs Backend in {
     val programTester = new ProgramTester(dir + "IOLoop.wacc")
     val (exitStatus, output) = programTester.run(programTester.testInput)
 
     exitStatus shouldBe programTester.expectedExitStatus.orElse(exitStatus)
     output shouldBe programTester.expectedOutput
-  }*/
+  }
 
-  it should "execute IOSequence.wacc" taggedAs Backend in pending /*{
+  it should "execute IOSequence.wacc" taggedAs Backend in {
     val programTester = new ProgramTester(dir + "IOSequence.wacc")
     val (exitStatus, output) = programTester.run(programTester.testInput)
 
     exitStatus shouldBe programTester.expectedExitStatus.orElse(exitStatus)
     output shouldBe programTester.expectedOutput
-  }*/
+  }
 
 }
