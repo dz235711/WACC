a23401d63e22929bdc820b81482326c6a5a17ecb
Rukaiya Khondaker <rnk23@ic.ac.uk>
2025-03-10 02:14:33 +0000

    test: add template for import unit tests and add initial tests

---
+++ src/test/wacc/custom/ImportsTests.scala
@@ -0,0 +1,46 @@
+package wacc
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
+import java.io.FileNotFoundException
+
+class ImportsTests extends AnyFlatSpec {
+  val dir = "src/test/examples/custom/imports"
+  val frontendErrorMessage = "Frontend failed to run"
+
+  // Testing whether a file can be imported, but not used
+  it should "provide unchanged AST when importing a file without using it" in {
+    val programFile = readFile(dir + "importUnused.wacc") match {
+      case None        => throw new FileNotFoundException()
+      case Some(lines) => lines
+    }
+    val programAST = runFrontend(programFile, false) match {
+      case Left(_)    => throw new Exception(frontendErrorMessage)
+      case Right(ast) => ast
+    }
+    compareFrontend(dir + "progWithoutImport.wacc", programAST) shouldBe true
+  }
+
+  // Testing whether a file can be imported and have functions used from it
+  it should "frontend analyse importAndUse.wacc" taggedAs (Frontend, Imports) in {
+    frontendStatus(dir + "importAndUse.wacc") shouldBe 0
+  }
+
+  // Testing whether multiple files can be imported
+
+  // Testing whether circular dependencies result in an error
+
+  // Testing whether nested imports work
+
+  // Testing whether an error is flagged for a non-existent file
+
+  // Testing behaviour when a file is imported multiple times
+
+  // Testing an error for if a function in a different file shares the same name as a function in the current file
+
+  // Testing whether a function with colliding names can be used if the file is specified i.e., call example.f()
+
+  // Testing whether an error is flagged if an imported file has syntax/semantic errors
+
+  // Testing whether an error is flagged if a file imports itself
+}
