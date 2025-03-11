ae7c50508e0a097356f62ca172dbf9fb158f8bf2
LawanF <fathullahlawan@gmail.com>
2025-03-11 14:41:45 +0000

    test: add declaration and assignment tests

--- src/test/testUtils.scala
+++ src/test/testUtils.scala
@@ -5,6 +5,7 @@ import org.scalatest.Tag
 
 object Frontend extends Tag("wacc.Frontend")
 object Backend extends Tag("wacc.Backend")
+object Repl extends Tag("wacc.Interpreter")
 
 // Timeout code for the timeout command
 val TimeoutCode = 124
